17de2f0e54dd6a3598ebf515f6f5bc2474cf1e70
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2023-11-10 07:09:23 -0500

    update

---
+++ Noviembre-09/HolaMundo.java
@@ -0,0 +1,25 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+        /* Datos individuales */
+        int numero = 1; // Entero
+        double promedio = 2; // decimales
+        char inicial = 'c'; // caracteres
+        String nombre = "Nestor";
+        boolean graduado = true;
+
+        /* Estructuras de datos */
+
+        int[] suerte = { 1, 3, 5, 7, 9 };
+        String[] lenguajes = { "Java", "Python", "JavaScript", "C++" };
+
+        double n = 10.99;
+        int edad = (int) n;
+
+        System.out.println("FIN");
+
+    }
+
+}
